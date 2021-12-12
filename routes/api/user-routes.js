b345b156f91fedd1cba8d02fe4954a0a91711dd4
chase-risinger <ccr1988@gmail.com>
2021-12-12 13:10:36 -0600

    create post model

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -54,6 +54,7 @@ router.post('/', (req, res) => {
 
 });
 
+// login to user
 router.post('/login', (req, res) => {
     // expects {email: 'lernantino@gmail.com', password: 'password1234'}
     User.findOne({
