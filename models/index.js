97181a8be197d8404e0dfe5e297e7bad41c917b9
chase-risinger <ccr1988@gmail.com>
2021-12-12 14:20:29 -0600

    Post routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations (one-2-many, one-2-one, many-2-many etc.)
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+//symmetry
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
