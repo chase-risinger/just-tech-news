b12896f4bffc82b1fa524d7a7f0a0404124ad0d0
chase-risinger <ccr1988@gmail.com>
2021-12-15 16:31:17 -0600

    Set up homepage with handlebars

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({})
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// express.static takes all the contents of a folder and serve them as static assets.
+app.use(express.static(path.join(__dirname, 'public')));
+
+// turn on handlebars 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
-// force: true is similar to drop table if exists. 
-// force false doens't trop table If we change the value of the force property to true, then the 
-// database connection must sync with the model definitions and associations. 
-// By forcing the sync method to true, we will make the tables re-create if there are any association changes.
+
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening'));
 });
